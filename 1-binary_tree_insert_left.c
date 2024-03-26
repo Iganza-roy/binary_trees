3fee0d4dbddcc1e64355414d828864cc46772d66
Roy Iganza Kibata <iganzaroy55@gmail.com>
2024-03-26 12:38:34 -0700

    added 1-binary_tree_insert_left.c

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -11,13 +11,13 @@
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_node;
-	
+
 	if (parent == NULL)
 		return (NULL);
 
 	new_node = malloc(sizeof(binary_tree_t));
 	if (new_node == NULL)
-		return(NULL);
+		return (NULL);
 
 	new_node->n = value;
 	new_node->parent = parent;
