ba062a8f63d2d6a080b2170a0df795b155b28196
Roy Iganza Kibata <iganzaroy55@gmail.com>
2024-03-28 13:07:23 -0700

    modified betty coding

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -31,6 +31,6 @@ binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 	if (node == NULL || node->parent == NULL || node->parent->parent == NULL)
 		return (NULL);
 
-	return binary_tree_sibling(node->parent);
+	return (binary_tree_sibling(node->parent));
 
 }
