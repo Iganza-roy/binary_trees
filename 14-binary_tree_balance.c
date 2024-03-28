a4eda2ac8619802fd7fcdfd270273f56d3519c3f
Roy Iganza Kibata <iganzaroy55@gmail.com>
2024-03-28 12:39:04 -0700

    added 14-binary_tree_balance.c

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,6 +1,26 @@
 #include "binary_trees.h"
 
 /**
+ * binary_tree_height - Measures the height of a binary tree
+ * @tree: Pointer to the root node of the tree to measure the height
+ * Return: Height of the binary tree, or 0 if tree is NULL
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+    size_t left_height, right_height;
+
+    if (tree == NULL)
+        return (0);
+
+    left_height = binary_tree_height(tree->left);
+
+    right_height = binary_tree_height(tree->right);
+
+    return (1 + ((left_height > right_height) ? left_height : right_height));
+}
+
+
+/**
  * binary_tree_balance - measures the balance factor of a binary tree
  * @tree: pointer to the root node of the tree to measure the balance factor
  * Return: Balance factor
