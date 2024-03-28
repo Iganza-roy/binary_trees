d02b42cf228e87f78ac19929b8a901833df16764
Roy Iganza Kibata <iganzaroy55@gmail.com>
2024-03-28 12:40:06 -0700

    added 14-binary_tree_balance.c

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -7,16 +7,16 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-    size_t left_height, right_height;
+	size_t left_height, right_height;
 
-    if (tree == NULL)
-        return (0);
+	if (tree == NULL)
+		return (0);
 
-    left_height = binary_tree_height(tree->left);
+	left_height = binary_tree_height(tree->left);
 
-    right_height = binary_tree_height(tree->right);
+	right_height = binary_tree_height(tree->right);
 
-    return (1 + ((left_height > right_height) ? left_height : right_height));
+	return (1 + ((left_height > right_height) ? left_height : right_height));
 }
 
 
@@ -34,6 +34,6 @@ int binary_tree_balance(const binary_tree_t *tree)
 	size_t left_height = binary_tree_height(tree->left);
 	size_t right_height = binary_tree_height(tree->right);
 
-	return (int)(left_height - right_height);
+	return ((int)(left_height - right_height));
 
 }
